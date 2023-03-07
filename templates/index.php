520b50b647ef90665abac7b512577ce1e6d1e7d8
Galtsevt Timofey <galtsev.t@yandex.ru>
2023-03-07 14:50:10 +0700

    gen time

--- templates/index.php
+++ templates/index.php
@@ -3,19 +3,56 @@
         <div class="">
             <h1></h1>
             <div class="">
-                Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus suscipit tortor eget felis
-                porttitor
-                volutpat. Donec rutrum congue leo eget malesuada. Cras ultricies ligula sed magna dictum porta.
-                Curabitur non
-                nulla sit amet nisl tempus convallis quis ac lectus.
 
-                Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Sed porttitor
-                lectus nibh.
-                Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ante ipsum primis in
-                faucibus orci
-                luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper
-                sit amet
-                ligula.
+
+                <p> Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit
+                    neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Lorem ipsum dolor sit amet,
+                    consectetur
+                    adipiscing elit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.<br>
+
+                <p> Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et
+                    ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet
+                    ligula. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat,
+                    accumsan
+                    id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non
+                    nisi.</p>
+
+                <p>Donec sollicitudin molestie malesuada. Proin eget tortor risus. Proin eget tortor risus. Nulla
+                    porttitor
+                    accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim.</p>
+                <p> Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit
+                    neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Lorem ipsum dolor sit amet,
+                    consectetur
+                    adipiscing elit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.<br>
+
+                <p> Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et
+                    ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet
+                    ligula. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat,
+                    accumsan
+                    id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non
+                    nisi.</p>
+
+                <p>Donec sollicitudin molestie malesuada. Proin eget tortor risus. Proin eget tortor risus. Nulla
+                    porttitor
+                    accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim.</p>
+                <p> Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit
+                    neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Lorem ipsum dolor sit amet,
+                    consectetur
+                    adipiscing elit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.<br>
+
+                <p> Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et
+                    ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet
+                    ligula. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat,
+                    accumsan
+                    id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non
+                    nisi.</p>
+
+                <p>Donec sollicitudin molestie malesuada. Proin eget tortor risus. Proin eget tortor risus. Nulla
+                    porttitor
+                    accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim.</p>
             </div>
         </div>
     </div>
--- templates/main.php
+++ templates/main.php
@@ -6,33 +6,41 @@
     <title>Главная страница</title>
     <?= vite('assets/js/app.js') ?>
 </head>
-<body>
-<header class="p-1 text-bg-dark">
-    <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-between py-2">
-            <a href="/" class="nav-link">Главная</a>
-            <div class="text-end">
-                <?php if (auth()->isAuthUser()): ?>
-                    <div class="d-flex">
-                        <a href="/home" type="button"
-                           class="btn btn-sm btn-warning me-2"><?= auth()->user()['login'] ?></a>
-                        <form action="/exit" method="POST">
-                            <button type="submit" class="btn btn-sm btn-outline-light me-2">Выход</button>
-                        </form>
-                    </div>
-                <?php else: ?>
-                    <a href="/login" type="button" class="btn btn-sm btn-outline-light me-2">Вход</a>
-                    <a href="/registration" type="button" class="btn btn-sm btn-warning">Регистрация</a>
-                <?php endif; ?>
+<body class="h-100">
+<div class="h-100 d-flex flex-column">
+    <header class="p-1 text-bg-dark shadow-sm">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-between py-2">
+                <a href="/" class="nav-link">Главная</a>
+                <div class="text-end">
+                    <?php if (auth()->isAuthUser()): ?>
+                        <div class="d-flex">
+                            <a href="/home" type="button"
+                               class="btn btn-sm btn-warning me-2"><?= auth()->user()['login'] ?></a>
+                            <form action="/exit" method="POST">
+                                <button type="submit" class="btn btn-sm btn-outline-light me-2">Выход</button>
+                            </form>
+                        </div>
+                    <?php else: ?>
+                        <a href="/login" type="button" class="btn btn-sm btn-outline-light me-2">Вход</a>
+                        <a href="/registration" type="button" class="btn btn-sm btn-warning">Регистрация</a>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
-    </div>
-</header>
-<main>
-    <div class="container">
-        <?= $mainContent ?>
-    </div>
-</main>
+    </header>
+    <main>
+        <div class="container">
+            <?= $mainContent ?>
+        </div>
+    </main>
+    <footer class="mt-auto">
+        <div class="container">
+            <div class="text-muted"><?= round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 4) ?> ms</div>
+        </div>
+    </footer>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
         crossorigin="anonymous"></script>
